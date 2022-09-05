fb973be7966bd370350f2f74ec69fd383063b195
Cristiane Santos <cristiane.dsc@yahoo.com>
2022-09-05 19:44:21 -0300

    Lista de Tarefas-Implementações 'Barra de rolagem vertical' e 'Validação tarefas vazias'  concluídas

--- to-do-list/assets/scripts.js
+++ to-do-list/assets/scripts.js
@@ -6,53 +6,52 @@ var botao = document.getElementById("botao");
 // Função que adiciona uma nova tarefa à lista
 function adicionaTarefa() {
 
-  let conteudo = document.createTextNode(entrada.value); // guarda conteúdo digitado pelo usuário
+  if (entrada.value) {  // validação que impede adição de tarefas vazias
+    let conteudo = document.createTextNode(entrada.value); // guarda conteúdo digitado pelo usuário
 
-  // cria novos elementos HTML
-  let linha = document.createElement('div');
-  let icone = document.createElement('div');
-  let tarefa = document.createElement('input');
-  let rotulo = document.createElement('label');
+    // cria novos elementos HTML
+    let linha = document.createElement('div');
+    let icone = document.createElement('div');
+    let tarefa = document.createElement('input');
+    let rotulo = document.createElement('label');
 
-  // define classes e tipos dos novos elementos criados
-  linha.className = 'linha-tarefa';
-  icone.className = 'icone-apagar';
-  tarefa.type = 'checkbox';
-  tarefa.className = 'tarefa';
+    // define classes e tipos dos novos elementos criados
+    linha.className = 'linha-tarefa';
+    icone.className = 'icone-apagar';
+    tarefa.type = 'checkbox';
+    tarefa.className = 'tarefa';
 
-  // inclui novos elementos criados dentro da div com id="listaTarefas"
-  listaTarefas.appendChild(linha);
-  linha.appendChild(icone);
-  linha.appendChild(tarefa);
-  linha.appendChild(rotulo);
-  rotulo.appendChild(conteudo);
+    // inclui novos elementos criados dentro da div com id="listaTarefas"
+    listaTarefas.appendChild(linha);
+    linha.appendChild(icone);
+    linha.appendChild(tarefa);
+    linha.appendChild(rotulo);
+    rotulo.appendChild(conteudo);
 
-  // conta o número de tarefas existentes (<input> dentro de listaTarefas)
-  quantidadeTarefas = document.querySelectorAll("#listaTarefas input").length;
+    // conta o número de tarefas existentes (<input> dentro de listaTarefas)
+    quantidadeTarefas = document.querySelectorAll("#listaTarefas input").length;
 
-  // define id do ícone apagar com base na quantidade de tarefas que já existem
-  icone.id = 'icone-apagar-' + quantidadeTarefas
+    // define id do ícone apagar com base na quantidade de tarefas que já existem
+    icone.id = 'icone-apagar-' + quantidadeTarefas
 
-  entrada.value = ''; // esvazia o campo de digitação
+    entrada.value = ''; // esvazia o campo de digitação
 
-  // Função anônima que remove tarefa a partir de detecção de eventos para clique no "X"
-  icone.addEventListener("click", () => {
-    linha.remove();
-  });
+    // Função anônima que remove tarefa a partir de detecção de eventos para clique no "X"
+    icone.addEventListener("click", () => {
+      linha.remove();
+    });
+  }
 }
 
 // Detecção de eventos que chamam adicionaTarefa() apenas se houver conteúdo na entrada
+
 entrada.addEventListener("keydown", function (e) {  // Enter
 
   let key = e.which || e.keyCode || 0;  // garante compatibilidade entre navegadores
 
-  if (entrada.value && key === 13) {
+  if (key === 13) {
     adicionaTarefa();
   }
 });
 
-botao.addEventListener("click", () => {  // clique/toque
-  if (entrada.value) {
-    adicionaTarefa();
-  }
-});
+botao.addEventListener("click", adicionaTarefa);  // clique/toque
